363ec11e513903f6c4580910a44d090fd719e3fe
David Almeida <djalmeida3@gmail.com>
2022-02-15 09:09:50 -0500

    Adding modifications practice

--- generators.py
+++ generators.py
@@ -1,6 +1,6 @@
 import time
 
-def fibo_gen():
+def fibo_gen(max: int = None):
     n1 = 0
     n2 = 1
     counter = 0
@@ -12,13 +12,17 @@ def fibo_gen():
             counter += 1
             yield n2
         else:
-            aux = n1 + n2
-            n1, n2 = n2, aux
-            counter += 1
-            yield aux
+            if counter < max:
+                aux = n1 + n2
+                n1, n2 = n2, aux
+                counter += 1
+                yield aux
+            else:
+                raise StopIteration
 
 if __name__ == '__main__':
-    fibonacci = fibo_gen()
+    max =  int(input("Ingrese el limite para la serie de Fibonacci: "))
+    fibonacci = fibo_gen(max)
     for element in fibonacci:
         print(element)
         time.sleep(.05)
