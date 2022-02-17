f10ddc7723449fb1d1078dcfecc8544199633df9
David Almeida <djalmeida3@gmail.com>
2022-02-17 09:13:33 -0500

    Mofications

--- sets.py
+++ sets.py
@@ -10,7 +10,7 @@ def remove_duplicates_with_sets(some_list):
 
 def run():
     random_list = [1, 2, 2, 2, 3, "Platzi", "Platzi", True, 4.6, False]
-    print(remove_duplicates(random_list))
+    print(remove_duplicates_with_sets(random_list))
 
 
 if __name__ == '__main__':
